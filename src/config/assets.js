cd8254602172c6dcae017ea6b74030636c67ea7b
Luis Carlos Ortega Gómez <lcog86@gmail.com>
2026-04-26 12:37:22 -0400

    Fix image issue

--- src/config/assets.js
+++ src/config/assets.js
@@ -1,80 +1,62 @@
+const pub = process.env.PUBLIC_URL;
+
 export const assets = {
-  logo: "img/logo.jpg",
+  logo: `${pub}/img/logo.jpg`,
   gallery: [
     {
-      largeImage: "img/portfolio/01-large.jpg",
-      smallImage: "img/portfolio/01-small.jpg"
+      largeImage: `${pub}/img/portfolio/01-large.jpg`,
+      smallImage: `${pub}/img/portfolio/01-small.jpg`
     },
     {
-      largeImage: "img/portfolio/02-large.jpg",
-      smallImage: "img/portfolio/02-small.jpg"
+      largeImage: `${pub}/img/portfolio/02-large.jpg`,
+      smallImage: `${pub}/img/portfolio/02-small.jpg`
     },
     {
-      largeImage: "img/portfolio/03-large.jpg",
-      smallImage: "img/portfolio/03-small.jpg"
+      largeImage: `${pub}/img/portfolio/03-large.jpg`,
+      smallImage: `${pub}/img/portfolio/03-small.jpg`
     },
     {
-      largeImage: "img/portfolio/04-large.jpg",
-      smallImage: "img/portfolio/04-small.jpg"
+      largeImage: `${pub}/img/portfolio/04-large.jpg`,
+      smallImage: `${pub}/img/portfolio/04-small.jpg`
     },
     {
-      largeImage: "img/portfolio/05-large.jpg",
-      smallImage: "img/portfolio/05-small.jpg"
+      largeImage: `${pub}/img/portfolio/05-large.jpg`,
+      smallImage: `${pub}/img/portfolio/05-small.jpg`
     },
     {
-      largeImage: "img/portfolio/06-large.jpg",
-      smallImage: "img/portfolio/06-small.jpg"
+      largeImage: `${pub}/img/portfolio/06-large.jpg`,
+      smallImage: `${pub}/img/portfolio/06-small.jpg`
     },
     {
-      largeImage: "img/portfolio/07-large.jpg",
-      smallImage: "img/portfolio/07-small.jpg"
+      largeImage: `${pub}/img/portfolio/07-large.jpg`,
+      smallImage: `${pub}/img/portfolio/07-small.jpg`
     },
     {
-      largeImage: "img/portfolio/08-large.jpg",
-      smallImage: "img/portfolio/08-small.jpg"
+      largeImage: `${pub}/img/portfolio/08-large.jpg`,
+      smallImage: `${pub}/img/portfolio/08-small.jpg`
     },
     {
-      largeImage: "img/portfolio/09-large.jpg",
-      smallImage: "img/portfolio/09-small.jpg"
+      largeImage: `${pub}/img/portfolio/09-large.jpg`,
+      smallImage: `${pub}/img/portfolio/09-small.jpg`
     }
   ],
   testimonials: [
-    {
-      img: "img/testimonials/01.jpg"
-    },
-    {
-      img: "img/testimonials/02.jpg"
-    },
-    {
-      img: "img/testimonials/03.jpg"
-    },
-    {
-      img: "img/testimonials/04.jpg"
-    },
-    {
-      img: "img/testimonials/05.jpg"
-    },
-    {
-      img: "img/testimonials/06.jpg"
-    }
+    { img: `${pub}/img/testimonials/01.jpg` },
+    { img: `${pub}/img/testimonials/02.jpg` },
+    { img: `${pub}/img/testimonials/03.jpg` },
+    { img: `${pub}/img/testimonials/04.jpg` },
+    { img: `${pub}/img/testimonials/05.jpg` },
+    { img: `${pub}/img/testimonials/06.jpg` }
   ],
   team: [
-    {
-      img: "img/team/01.jpg"
-    },
-    {
-      img: "img/team/02.jpg"
-    },
-    {
-      img: "img/team/03.jpg"
-    },
-    {
-      img: "img/team/04.jpg"
-    }
+    { img: `${pub}/img/team/01.jpg` },
+    { img: `${pub}/img/team/02.jpg` },
+    { img: `${pub}/img/team/03.jpg` },
+    { img: `${pub}/img/team/04.jpg` }
   ],
   social: {
     facebook: "https://www.facebook.com/solutionsnplusone",
     twitter: "https://twitter.com/solutionsnplus1",
     youtube: "https://www.youtube.com/@solutionsnplusone"
   }
-}; 
+};
